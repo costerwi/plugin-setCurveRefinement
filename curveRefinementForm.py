1b6cf8128eb7151ed18008141a7fd48729a352d3
Carl Osterwisch <costerwi@gmail.com>
2024-06-29 12:13:37 -0400

    Simplify many methods and make more tolerant of part name characters

--- curveRefinementForm.py
+++ curveRefinementForm.py
@@ -1,61 +1,31 @@
 from abaqusGui import *
-from abaqusConstants import EXTRA_COARSE, COARSE, MEDIUM, FINE, EXTRA_FINE
-
-###########################################################################
-# Class definition
-###########################################################################
+from abaqusConstants import COARSE
+from curveRefinementDB import curveRefinementDB
 
 class curveRefinementForm(AFXForm):
+    """Associate the dialog box with a kernel command and parameters (keywords)"""
 
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def __init__(self, owner):
+        AFXForm.__init__(self, owner)
 
-        self.owner = owner
-        AFXForm.__init__(self, owner=owner)
-        self.cmd = AFXGuiCommand(self,'setCurveRefinement','curveRefinementUtils',registerQuery=False)
-        self.optionsKw = AFXSymConstKeyword(command=self.cmd,name='options',isRequired=True,defaultValue=COARSE.getId())
-        self.partsKw =AFXTupleKeyword(command=self.cmd,name='parts',isRequired=True,opts=AFXTUPLE_TYPE_STRING)
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        cmd = AFXGuiCommand(
+                mode=self,
+                method='setCurveRefinement',
+                objectName='curveRefinementUtils')
+
+        self.refinementKw = AFXSymConstKeyword(
+                command=cmd,
+                name='refinement',
+                isRequired=True,
+                defaultValue=COARSE.getId())
+
+        self.partsKw = AFXTupleKeyword(
+                command=cmd,
+                name='parts',
+                isRequired=True,
+                opts=AFXTUPLE_TYPE_STRING)
+
+
     def getFirstDialog(self):
-
         """Return the first dialog to be posted"""
-        from curveRefinementDB import curveRefinementDB
-        self.db = curveRefinementDB(self)
-        return self.db
-
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    def activate(self):
-        mainWindow = getAFXApp().getAFXMainWindow()
-        try:
-            self.modelName = session.viewports[session.currentViewportName].displayedObject.modelName
-        except:
-            msg='There is no model database (MDB) \n'\
-                'associated with the current viewport.\n'
-            showAFXErrorDialog(mainWindow,msg)
-            return
-        if not list(mdb.models[self.modelName].parts.keys()):
-            showAFXErrorDialog(mainWindow,'No parts available in the current model\n')
-        else:
-            AFXForm.activate(self)
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    def deactivate(self):
-        mainWindow = getAFXApp().getAFXMainWindow()
-        AFXForm.deactivate(self)
-
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    def verifyKeywordValues(self):
-        mainWindow = getAFXApp().getAFXMainWindow()
-        numItems = self.db.list1.getNumItems()
-
-        if numItems == 0:
-            showAFXErrorDialog(mainWindow,'No parts available in the current model\n')
-            return 0
-        Flag=False
-        for i in range(numItems):
-            if self.db.list1.isItemSelected(i):
-                Flag=True
-                break
-        if Flag == False:
-            showAFXErrorDialog(mainWindow,'Please select at least one part.')
-            return 0
-        return 1
+        return curveRefinementDB(self)
